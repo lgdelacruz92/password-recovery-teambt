3569a025c95aab384a3910052517dbf82383aea0
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-16 10:35:09 -0800

    Added dynamic width

--- src/PasswordRecovery/passwordrecovery.js
+++ src/PasswordRecovery/passwordrecovery.js
@@ -3,14 +3,25 @@ import * as MUI from "@material-ui/core";
 
 const useStyles = MUI.makeStyles(theme => {
   return {
-    passwordRecovery: {}
+    passwordRecovery: {
+      width: 350,
+      padding: 20,
+      "@media only screen and (max-width: 600px)": {
+        width: "80%",
+        padding: "1em"
+      }
+    }
   };
 });
 
 const PasswordRecovery = props => {
   const classes = useStyles();
   const { children } = props;
-  return <div className={classes.passwordRecovery}>{children}</div>;
+  return (
+    <MUI.Paper elevation={3} className={classes.passwordRecovery}>
+      {children}
+    </MUI.Paper>
+  );
 };
 
 export default PasswordRecovery;
